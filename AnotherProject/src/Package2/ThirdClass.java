289c1237a56ae22f3a51c0a7c64dfa325c3f8875
anilpawarmamidi <anilpawaronline@gmail.com>
2019-12-01 17:28:28 -0500

    another second line in third class

--- AnotherProject/src/Package2/ThirdClass.java
+++ AnotherProject/src/Package2/ThirdClass.java
@@ -5,6 +5,7 @@ public class ThirdClass {
 	public static void main(String[] args) {
 		
 		System.out.println("This is the third class");
+		System.out.println("second line in thrird class");
 
 	}
 
