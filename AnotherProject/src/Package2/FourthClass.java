cc370d7ebc7189af012ee54fdde999c29613c0e0
anilpawarmamidi <anilpawaronline@gmail.com>
2019-12-01 23:32:40 -0500

    fourth class added

---
+++ AnotherProject/src/Package2/FourthClass.java
@@ -0,0 +1,17 @@
+package Package2;
+
+public class FourthClass {
+
+	int a=10, b=20, d;
+	
+	public void add()
+	{
+		d=a+b;
+		System.out.println("The addition of A and B is " + d);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
